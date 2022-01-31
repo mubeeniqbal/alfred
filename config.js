105a9b3ab182447951f4159f1ca16365af8d2717
Mubeen Iqbal <mubeen.ace@gmail.com>
2022-01-31 07:17:29 +0500

    Add lobby to .env

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 const env = process.env.NODE_ENV;
+
+// Discord bot token.
 const token = process.env.TOKEN;
 
-export { env, token };
+// Discord channels.
+const lobby = process.env.LOBBY;
+
+export { env, token, lobby };
